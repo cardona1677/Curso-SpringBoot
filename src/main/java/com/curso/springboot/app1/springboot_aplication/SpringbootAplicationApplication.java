8a93a85168d1995be6f1508b565a52a1753f86cb
alejandrocorreac <cardonacocristian285@gmail.com>
2026-02-17 15:43:16 -0500

    Encarpetado spring boot

---
+++ src/main/java/com/curso/springboot/app1/springboot_aplication/SpringbootAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.app1.springboot_aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAplicationApplication.class, args);
+	}
+
+}
