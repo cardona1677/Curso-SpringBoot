71bd7519a17c0336ae7d7eb560746c8763a5c564
alejandrocorreac <cardonacocristian285@gmail.com>
2026-02-17 16:31:39 -0500

    creacion de controller

---
+++ src/main/java/com/curso/springboot/app1/springboot_aplication/controller/EjemploController.java
@@ -0,0 +1,15 @@
+package com.curso.springboot.app1.springboot_aplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class EjemploController {
+
+    @GetMapping("/detalles_info")
+    public String info() {
+        return "detalles_info";
+    }
+
+}
