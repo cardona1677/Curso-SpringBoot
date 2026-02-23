9ec8453904642788664661d002d760a80b1bf1a7
alejandrocorreac <cardonacocristian285@gmail.com>
2026-02-23 15:45:04 -0500

    Creacion de Models y pasar datos a una vista

--- src/main/java/com/curso/springboot/app1/springboot_aplication/controller/EjemploController.java
+++ src/main/java/com/curso/springboot/app1/springboot_aplication/controller/EjemploController.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.curso.springboot.app1.springboot_aplication.models.Carros;
+
 
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
     public String info(Model model) {
-        model.addAttribute("Titulo", "Drag Racing Details");
-        model.addAttribute("Descripcion", "Drag racing es una forma de competición automovilística en la que dos vehículos compiten en una carrera de aceleración en línea recta, generalmente en una pista de un cuarto de milla (402 metros). El objetivo es llegar a la meta lo más rápido posible, y el ganador es el que cruza la línea de meta primero. El drag racing se caracteriza por su alta velocidad y la emoción que genera entre los espectadores.");
-        model.addAttribute("Imagen", "https://dragillustrated.com/wp-content/uploads/2024/11/PDRA-schedule_Tara-Bowker-photo_DI-web-1024x576.jpg");
+        Carros carro2 = new Carros(2L, "Chevrolet", "Nova", 1971, "Murder Nova");
+        model.addAttribute("Carro", carro2);
         return "detalles_info";
     }
 
--- src/main/java/com/curso/springboot/app1/springboot_aplication/controller/EjemploRestController.java
+++ src/main/java/com/curso/springboot/app1/springboot_aplication/controller/EjemploRestController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.springboot.app1.springboot_aplication.models.Carros;
+
 
 @RestController
 @RequestMapping("/api")
 public class EjemploRestController {
+    Carros carro1 = new Carros(1L, "Chevrolet", "Nova", 1969, "Goliath 2.0");
+    // Carros carro2 = new Carros(2L, "Chevrolet", "Nova", 1971, "Murder Nova");
 
     @GetMapping("/detalles_info2")
     public Map<String, Object> detalles_info2() {
         Map<String, Object> respuesta = new HashMap<>();
         respuesta.put("Titulo", "Drag Racing Details");
+        respuesta.put("Carro", carro1);
         respuesta.put("Descripcion", "Drag racing es una forma de competición automovilística en la que dos vehículos compiten en una carrera de aceleración en línea recta, generalmente en una pista de un cuarto de milla (402 metros). El objetivo es llegar a la meta lo más rápido posible, y el ganador es el que cruza la línea de meta primero. El drag racing se caracteriza por su alta velocidad y la emoción que genera entre los espectadores.");
         respuesta.put("Imagen", "https://dragillustrated.com/wp-content/uploads/2024/11/PDRA-schedule_Tara-Bowker-photo_DI-web-1024x576.jpg");
         return respuesta;
