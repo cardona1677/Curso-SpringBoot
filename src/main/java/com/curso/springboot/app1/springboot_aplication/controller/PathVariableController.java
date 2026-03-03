9e1cf047336f652c4b088e23d790c75326bb193f
alejandrocorreac <cardonacocristian285@gmail.com>
2026-03-03 15:58:23 -0500

    enviar solicitudes post con postman

--- src/main/java/com/curso/springboot/app1/springboot_aplication/controller/PathVariableController.java
+++ src/main/java/com/curso/springboot/app1/springboot_aplication/controller/PathVariableController.java
@@ -2,11 +2,15 @@ package com.curso.springboot.app1.springboot_aplication.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.springboot.app1.springboot_aplication.models.Carros;
+
 @RestController
-@RequestMapping("/api/pathvariable")
+@RequestMapping("/api/variable")
 public class PathVariableController {
 
     @GetMapping("/pagina1/{mensaje}")
@@ -15,5 +19,12 @@ public class PathVariableController {
         parametro1.setInformacion(mensaje);
         return parametro1;
     }
+
+    @PostMapping("/solicitud")
+    public Carros crearCarro(@RequestBody Carros carro) {
+        
+        return carro;
+    }
+
 
 }
